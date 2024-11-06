747cc1b30232ea7231305b3088005a16a5d65c4a
Edwin <moyiedwin8@gmail.com>
2024-11-06 21:46:14 +0300

    naming error

--- server/models/userclub.py
+++ server/models/userclub.py
@@ -1,5 +1,4 @@
 from models.db import db 
-from datetime import datetime
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.sql import func
 
@@ -7,7 +6,7 @@ class UserClub(db.Model,SerializerMixin):
     __tablename__ = 'club_users'
 
     serialize_rules = ('-user_id',)
-    movie_club_id = db.Column(db.Integer, db.ForeignKey('movie_clubs.id'), primary_key=True)
+    movie_club_id = db.Column(db.Integer, db.ForeignKey('clubs.id'), primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
     joined_at = db.Column(db.DateTime(timezone=True), server_default=func.now(), nullable=False)
 
