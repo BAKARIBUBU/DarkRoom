e8592212e7f2f0d9be5f03cf1c725859dac2cdc8
+Sharon <+wanjiku.kahira@gmail.com>
2024-11-09 11:03:09 +0300

    Delete relationships and add new relationships

--- server/models/userclub.py
+++ server/models/userclub.py
@@ -6,12 +6,13 @@ from sqlalchemy.sql import func
 class UserClub(db.Model,SerializerMixin):
     __tablename__ = 'club_users'
 
-    serialize_rules = ('-user_id',)
-    movie_club_id = db.Column(db.Integer, db.ForeignKey('movie_clubs.id'), primary_key=True)
+    serialize_rules = ('-user_id','-club',)
+
+    club_id = db.Column(db.Integer, db.ForeignKey('clubs.id'), primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
     joined_at = db.Column(db.DateTime(timezone=True), server_default=func.now(), nullable=False)
 
-    club = db.relationship('Club', back_populates='users')
-    user = db.relationship('User', back_populates='clubs')
+    club = db.relationship('Club', back_populates='club_users')
+    user = db.relationship('User', back_populates='club_users')
 
 
