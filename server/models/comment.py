f9d49f75e0e291322efcb427efc241b36e10c455
+Sharon <+wanjiku.kahira@gmail.com>
2024-11-11 19:56:13 +0300

    Modify code in comments.py

--- server/models/comment.py
+++ server/models/comment.py
@@ -5,7 +5,7 @@ from sqlalchemy.sql import func
 class Comment(db.Model,SerializerMixin):
     __tablename__ = 'comments'
 
-    serialize_rules = ('-post_id','-user_id',)
+    serialize_rules = ('-post','-user',)
     
     id = db.Column(db.Integer, primary_key=True)
     post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), nullable=False)
